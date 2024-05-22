21f27353c00527466e622ac479ab1382167af896
phongyv <321phong@gmail.com>
2024-05-22 22:47:57 +0700

    22/05/2024

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './page/Home.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
+<BrowserRouter>
+<Routes>
+<Route path='' element={<Home/>}></Route>
+</Routes>
+</BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/page/Message.js
@@ -0,0 +1,10 @@
+
+function Message(){
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Message
