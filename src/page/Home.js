b5fb4442ec1f0168a3bac5bb062205ec8e8335df
phongyv <321phong@gmail.com>
2024-05-23 22:14:35 +0700

    heelod

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../css/Home.css'
 
 function Home(){
@@ -12,15 +13,24 @@ function Home(){
             <input type='text' placeholder='Search' className='home-navbar-left-search'></input>
           <img className='home-navbar-left-submit' src="arrow.png" alt="" />
         </div>
-        <img className='home-navbar-center' src="home.png" alt="" />
+
+        <Link className='home-navbar-link' to={'/home'}>
+        <img className='home-navbar-center' src="home.png" alt=""/>
+        </Link>
+
         <div className='home-navbar-right'>
+            <Link className='home-navbar-link' to={'/home/message'}>
             <img className='home-navbar-right-message' src='messenger.png' alt='' />
+            </Link>
+            <Link className='home-navbar-link' to={'/home/user'}>
             <div className='home-navbar-right-user' >
             <img  src='user.png' alt='' />
             <span>{username}</span>
             </div>
+            </Link>
         </div>
        </div>
+
        <div className='home-body'>
         
        </div>
